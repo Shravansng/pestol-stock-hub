17e8088032e2d7b9f09246b3e00a7b24d37c71ef
Shravansng <124402520+Shravansng@users.noreply.github.com>
2026-03-14 11:40:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/valentine-s-vows/", // <--- ADD THIS LINE
+})
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/pestol-stock-hub/"
   server: {
     host: "::",
     port: 8080,
